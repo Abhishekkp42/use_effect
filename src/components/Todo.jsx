bfe7e2db6bd960abe17b8ddba01c857e34a666f6
Abhishekkp42 <96106223+Abhishekkp42@users.noreply.github.com>
2022-03-13 21:35:25 +0530

    completed

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,12 +1,17 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import axios from "axios"
 
 export const Todo = () => {
 	const [text, setText] = useState("")
 	const [list, setList] = useState([])
+	const [page, setPage] = useState(1)
+
+	useEffect(() => {
+		getText();
+	},[page])
 
 	const getText= () =>{
-		axios.get("http://localhost:3001/posts").then((res) => {
+		axios.get(`http://localhost:3001/posts?_limit=2&_page=${page}`).then((res) => {
 			setList(res.data)
 		})
 	}
@@ -28,9 +33,22 @@ export const Todo = () => {
 					getText();
 				})
 			}}>Add Todo</button>
-			{list.map((e) => {
-				<div key={e.id}>{e.title}</div>
-			})}
+
+			{list.map((e) => (
+				<div key={e.id}>
+				<p>{e.title}</p>
+				<button>Delete</button>
+				</div>
+	))}
+
+			<button onClick={() => {
+				setPage(page-1)
+			}}>prev</button>
+
+			<button onClick={() => {
+					setPage(page+1)
+				}}>next</button>
+
 		</div>
 	)
 }
